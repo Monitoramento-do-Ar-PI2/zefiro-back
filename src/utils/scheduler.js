a6ac6992e6266f4ad01e7222c919c50c3c31f38b
Calebe Rios <calebersmendes@gmail.com>
2020-11-12 20:08:56 -0300

    Test for apresentation

--- src/utils/scheduler.js
+++ src/utils/scheduler.js
@@ -6,13 +6,13 @@ const client = mqtt.connect('mqtt://localhost:1883');
 
 const ZefiroScheduler = {
   setSchedulerTo00() {
-    schedule.scheduleJob('*/2 * * * *', () => {
+    schedule.scheduleJob('*/2 * * * * *', () => {
       const date = new Date();
-      const minutes = date.getMinutes();
+      const minutes = date.getSeconds();
 
       if ((minutes % 10 !== 0)
-       && (minutes !== 6 || minutes !== 16 || minutes !== 26
-       || minutes !== 36 || minutes !== 46 || minutes !== 56)) {
+      && (minutes !== 6 && minutes !== 16 && minutes !== 26
+      && minutes !== 36 && minutes !== 46 && minutes !== 56)) {
         StationModel.findAll().then((stations) => {
           stations.forEach((station) => {
             client.publish(`Zefiro/${station.name}`, '00');
@@ -22,7 +22,7 @@ const ZefiroScheduler = {
     });
   },
   setSchedulerTo01() {
-    schedule.scheduleJob('10,20,30,40,50 * * * *', () => {
+    schedule.scheduleJob('10,20,30,40,50 * * * * *', () => {
       StationModel.findAll().then((stations) => {
         stations.forEach((station) => {
           client.publish(`Zefiro/${station.name}`, '01');
@@ -31,7 +31,7 @@ const ZefiroScheduler = {
     });
   },
   setSchedulerTo10() {
-    schedule.scheduleJob('0 6-18 * * *', () => {
+    schedule.scheduleJob('0 * 6-18 * * *', () => {
       StationModel.findAll().then((stations) => {
         stations.forEach((station) => {
           client.publish(`Zefiro/${station.name}`, '10');
@@ -40,7 +40,7 @@ const ZefiroScheduler = {
     });
   },
   setSchedulerTo11() {
-    schedule.scheduleJob('0 19-23,0-5 * * *', () => {
+    schedule.scheduleJob('0 * 19-23,0-5 * * *', () => {
       StationModel.findAll().then((stations) => {
         stations.forEach((station) => {
           client.publish(`Zefiro/${station.name}`, '11');
